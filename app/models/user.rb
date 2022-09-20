7d18b02b3925058b6cf5ff1a8308afa0c3969587
Kade Illian <kadeillianmt@gmail.com>
2022-09-20 11:00:27 -0600

    changed user.rb to has_secure_password, seeded

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :projects
   has_many :tiers, through: :pledges
   has_many :pledges
+  has_secure_password
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# User.create!(
+#   name: "Joe",
+#   email: "joe@test.com",
+#   password: "password",
+#   image: "img.jpeg",
+# )
+
+# User.create!(
+#   name: "bob",
+#   email: "bob@test.com",
+#   password: "password",
+#   image: "img.jpeg",
+# )
+
+# User.create!(
+#   name: "jim",
+#   email: "jim@test.com",
+#   password: "password",
+#   image: "img.jpeg",
+# )
+
+Project.create!(
+    title: "Faze",
+    description: "This is Faze clan.",
+    goal_amount: 10000,
+    start_date: 10/01/2022,
+    end_date: 11/01/2022,
+)
+
+# Project.create!(
+#   title: "Faze",
+#   description: "This is Faze clan.",
+#   goal_amount: 10000,
+#   start_date: 10/01/2022,
+#   end_date: 11/01/2022,
+# )
