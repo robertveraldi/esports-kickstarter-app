499fb8987cc26089ca9849bf6016ec1ac570920b
Kade Illian <kadeillianmt@gmail.com>
2022-09-21 10:49:23 -0600

    basic authentication using cheatsheet working

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
   get "/signup" => "users#new"
   post "/users" => "users#create"
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
