31fbb162d2c795c08a5189f81d12100131828764
Kade Illian <kadeillianmt@gmail.com>
2022-09-21 11:34:53 -0600

    added the ability to see users, and for current users to edit or delete their accounts

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def destroy_user(user)
+    if user == current_user
+      button_to "Destroy this user", user, method: :delete
+    end
+  end
+
+  def edit_user(user)
+    if user == current_user
+      link_to "Edit this user", edit_user_path(user)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "projects#index"
   resources :projects
+  resources :users
 
   get "/signup" => "users#new"
   post "/users" => "users#create"
