12e413644f0cc0433a1cbdf5678eda7389c26d42
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-09-22 12:38:05 -0400

    added user_id column to project model, defined associations between user and project models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :tiers, through: :pledges
   has_many :pledges
+  has_many :projects
   has_secure_password
   validates :email, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_21_174553) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_163117) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_21_174553) do
     t.datetime "updated_at", null: false
     t.text "logo"
     t.decimal "current_amount", precision: 9, scale: 2
+    t.integer "user_id"
   end
 
   create_table "tiers", force: :cascade do |t|
@@ -66,5 +67,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_21_174553) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
