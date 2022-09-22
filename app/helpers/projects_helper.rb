3ccfb5bc482ef8877b09ff7abd9272d9e1302efe
Kade Illian <kadeillianmt@gmail.com>
2022-09-22 12:44:30 -0600

    working on show

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,4 +2,11 @@ module ProjectsHelper
   def update_current_amount
     @project.current amount
   end
+
+  def edit_project(project)
+    if current_user&.id == project.user.id
+      link_to "Edit this Project", edit_project_path(project)
+    end
+  end
+
 end
