2cbd7a9d33a931e916d6f374041095a35ba3b247
Lucas Duffy <lucas.a.duffy@gmail.com>
2022-09-23 15:01:06 -0400

    completed project show page

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,20 +1,19 @@
 module ProjectsHelper
-
   def edit_project(project)
     if current_user&.id == project.user.id
-      link_to "Edit this Project", edit_project_path(project)
+      link_to "Edit this Project", edit_project_path(project), class: "btn btn-primary btn-lg btn-block mx-2 my-2"
     end
   end
 
   def delete_project(project)
     if current_user&.id == project.user.id
-      button_to "Delete this Project", project, method: :delete
+      button_to "Delete this Project", project, method: :delete, class: "btn btn-primary btn-lg btn-block mx-2 my-2"
     end
   end
 
   def add_tier
-    if current_user&.id == @project.user.id 
-      button_to "Add Tier", new_tier_path, method: :get
+    if current_user&.id == @project.user.id
+      button_to "Add Tier", new_tier_path, method: :get, class: "btn btn-primary btn-lg btn-block mx-2 my-2"
     end
   end
 end
