7fdecf2d8539dbd3f7d63c8a89fc1eda96bb0796
Kade Illian <kadeillianmt@gmail.com>
2022-09-23 10:37:24 -0600

    added current_amount method to project.rb

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,4 +1,6 @@
 class PledgesController < ApplicationController
+  before_action :set_tiers, only: %i[ new create ]
+
   def show
   end
 
@@ -6,8 +8,16 @@ class PledgesController < ApplicationController
   end
 
   def create
+    @pledge = Pledge.new(tier_id: @tiers.id, user_id: current_user.id)
+    @pledge.save
   end
 
   def delete
+  end
+
+  private
+
+  def set_tiers
+    @tiers = Project.find_by(id: params[:id]).tiers
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,12 @@ class Project < ApplicationRecord
   has_many :project_categories
   has_many :tiers
   belongs_to :user
+
+  def current_amount
+    @current_amount = 0
+    tiers.each do |tier|
+      @current_amount += tier.amount * tier.pledges.length
+    end
+    return @current_amount
+  end
 end
